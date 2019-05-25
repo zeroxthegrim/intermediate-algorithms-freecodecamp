89eaaca4e4aef9ded81130a28567c13d6788a03b
Niksa <nhribljan@gmail.com>
2019-05-25 10:32:55 +0200

    Add files via upload

---
+++ sum-all-odd-fibbonaci-numbers.js
@@ -0,0 +1,30 @@
+/*
+Given a positive integer num, return the sum of all odd Fibonacci numbers 
+that are less than or equal to num.
+The first two numbers in the Fibonacci sequence are 1 and 1.
+Every additional number in the sequence is the sum of the two previous numbers. 
+The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than 
+or equal to 10 are 1, 1, 3, and 5.
+*/
+
+
+
+function sumFibs(num) {
+   let fibbArr = [1, 1];
+   while(fibbArr[fibbArr.length - 1] <= num) {
+      fibbArr.push(fibbArr[fibbArr.length - 2] + fibbArr[fibbArr.length - 1]);
+   }
+   if (fibbArr[fibbArr.length - 1] > num) {
+      fibbArr.pop();
+   }
+   return fibbArr
+      .filter(num => {
+      return num % 2 != 0;
+   })
+      .reduce((a, b) => {
+         return a + b;
+      }, 0)
+ }
+ 
+ console.log(sumFibs(4));
